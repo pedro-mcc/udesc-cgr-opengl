d1f6e9d4882a0ea02b016f1b442fe1a71de836e3
Pedro M <102629913+pedro-mcc@users.noreply.github.com>
2026-04-15 12:46:15 -0300

    deleted

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,51 @@
 // gcc main.cpp -lglfw -lGL -lGLU -lm -o particles && ./particles
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <iostream>
-#include <vector>
-#include <random>
-#include <Particle.h>
+#include <particle.h>
+#include <window.h>
 
+int main() {
+    GLFWwindow* window = initWindow(1280, 720);
 
-GLFWwindow* initWindow(int w, int h){
-    // cerr: acts as an unbuffered standar error stream
-    if(!glfwInit()) { 
-        std::cerr << "WLFW init failed\n"; exit(1); 
+    // --- shader source strings (or load from file) ---
+    const char* vertSrc = particle.vert;
+    const char* fragSrc = /* ... paste particle.frag ... */;
+    GLuint shader = createProgram(vertSrc, fragSrc);
+
+    setupBuffers();
+    initParticles();
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE);  // additive blending → glow effect
+
+    glm::mat4 proj = glm::perspective(glm::radians(60.f), 1280.f/720.f, 0.1f, 100.f);
+    glm::mat4 view = glm::lookAt(glm::vec3(0,2,8), glm::vec3(0,1,0), glm::vec3(0,1,0));
+    glm::mat4 mvp  = proj * view;
+
+    double lastTime = glfwGetTime();
+
+    while (!glfwWindowShouldClose(window)) {
+        double now = glfwGetTime();
+        float  dt  = float(now - lastTime);
+        lastTime   = now;
+
+        updateParticles(dt);
+        uploadParticles();
+
+        glClearColor(0.02f, 0.02f, 0.05f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glUniformMatrix4fv(glGetUniformLocation(shader, "uMVP"),
+                           1, GL_FALSE, &mvp[0][0]);
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_POINTS, 0, MAX_PARTICLES);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    GLFWwindow* window = glfwCreateWindow(w, h, "Particle System", nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-
-    glewExperimental = GL_TRUE;
-    if(glewInit() != GLEW_OK) { 
-        std::cerr << "GLEW init failed\n"; exit(1); 
-    }
-    glViewport(0, 0, w, h);
-    
-    return window;
+    glfwTerminate();
+    return 0;
 }
-
